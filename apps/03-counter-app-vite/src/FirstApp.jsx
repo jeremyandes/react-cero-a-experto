0a9ef346d84b99e2c9f78fc8268b6940940dec7f
jeremyandes <jeremyandes25@gmail.com>
2022-06-22 17:09:52 -0300

    40. Comunicación entre componentes - Props

--- apps/03-counter-app-vite/src/FirstApp.jsx
+++ apps/03-counter-app-vite/src/FirstApp.jsx
@@ -1,13 +1,9 @@
-const getResult = (a, b) => {
-    return a + b;
-}
-
-export const FirstApp = () => {
+export const FirstApp = ({ title, subtitle }) => {
     return (
         <>
-            <h1>{getResult(1, 2)}</h1>
+            <h1>{title}</h1>
             {/* <code>{JSON.stringify(newMessage)}</code> */}
-            <h4>Soy un subtítulo</h4>
+            <h4>{subtitle}</h4>
         </>
     )
 }
--- apps/03-counter-app-vite/src/main.jsx
+++ apps/03-counter-app-vite/src/main.jsx
@@ -8,6 +8,6 @@ import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <FirstApp />
+        <FirstApp title='Hola, soy Jeremy' subtitle='Soy un subtítulo' />
     </React.StrictMode>
 );
