98fbe39eddd5eee554f22109b89a05281a7968e0
jeremyandes <jeremyandes25@gmail.com>
2022-06-22 16:33:36 -0300

    35. Nuestro primer Componente

--- apps/03-counter-app-vite/src/main.jsx
+++ apps/03-counter-app-vite/src/main.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { App } from './App'
 
-const App = () => {
-    return (
-        <h1>Hola mundo!</h1>
-    )
-}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
