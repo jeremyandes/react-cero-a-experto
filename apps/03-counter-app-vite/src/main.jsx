fa8de6de7bd121598dafa69a06ffbf1395249444
jeremyandes <jeremyandes25@gmail.com>
2022-06-22 16:56:37 -0300

    39. Colocar estilos de CSS

--- apps/03-counter-app-vite/src/main.jsx
+++ apps/03-counter-app-vite/src/main.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 // import { App } from './App';
-import { FirstApp } from './FirstApp'
+import { FirstApp } from './FirstApp';
+
+import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
