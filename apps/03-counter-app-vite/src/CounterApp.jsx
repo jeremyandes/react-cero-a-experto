cd194434837b22bb93496d61b3a4a6ace3aa3b44
jeremyandes <jeremyandes25@gmail.com>
2022-06-22 21:20:45 -0300

    44. Evento click (Eventos en general)

--- apps/03-counter-app-vite/src/CounterApp.jsx
+++ apps/03-counter-app-vite/src/CounterApp.jsx
@@ -1,10 +1,15 @@
 import PropTypes from 'prop-types'
 
 export const CounterApp = ({ value }) => {
+    const clickEvent = (event) => console.log(event);
+
     return (
         <>
             <h1>CounterApp</h1>
             <h2>{value}</h2>
+            <button onClick={clickEvent}>
+                +1
+            </button>
         </>
     )
 }
