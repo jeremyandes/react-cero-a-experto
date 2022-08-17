e50fd7092de4d038b6bc0d8cc0fed35af0f53bb7
jeremyandes <jeremyandes25@gmail.com>
2022-08-17 19:47:37 -0300

    63. Probar FirstApp

--- apps/03-counter-app-vite/tests/FirstApp.test.jsx
+++ apps/03-counter-app-vite/tests/FirstApp.test.jsx
@@ -5,8 +5,20 @@ describe('Pruebas en <FirstApp />', () => {
 
     test('Debe hacer match con el snapshot', () => {
 
-        render(<FirstApp />)
+        const title = 'Hola, soy Jeremy';
+        const { container } = render(<FirstApp title={title} />);
+        expect(container).toMatchSnapshot();
 
+    })
+
+    test('Debe mostrar el título en un H1', () => {
+
+        const title = 'Jeremy';
+        const { container, getByText } = render(<FirstApp title={title} />);
+        expect(getByText(title)).toBeTruthy();
+
+        const h1 = container.querySelector('h1');
+        expect(h1.innerHTML).toContain(title);
     })
 
 })
