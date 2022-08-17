0000a9f8e40a278d44fb0b5fe7d4660bd2fcae2d
jeremyandes <jeremyandes25@gmail.com>
2022-08-17 19:55:08 -0300

    64. getByTestId y otras props

--- apps/03-counter-app-vite/src/FirstApp.jsx
+++ apps/03-counter-app-vite/src/FirstApp.jsx
@@ -4,8 +4,10 @@ export const FirstApp = ({ title, subtitle }) => {
 
     return (
         <>
-            <h1>{title}</h1>
+            <h1 data-testid='test-title'>{title}</h1>
             {/* <code>{JSON.stringify(newMessage)}</code> */}
+            <h4>{subtitle}</h4>
+            <h4>{subtitle}</h4>
             <h4>{subtitle}</h4>
         </>
     );
