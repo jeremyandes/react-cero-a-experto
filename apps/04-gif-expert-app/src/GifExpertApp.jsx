4273aed881270a6438768c6b1cd12f78448b1fca
jeremyandes <jeremyandes25@gmail.com>
2022-08-18 20:29:09 -0300

    73. GifExpertApp - Component

--- apps/04-gif-expert-app/src/GifExpertApp.jsx
+++ apps/04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,7 +1,14 @@
 export const GifExpertApp = () => {
     return (
         <>
+            {/* Titulo */}
             <h1>GifExpertApp</h1>
+
+            {/* Input */}
+
+            {/* Gifs */}
+            {/* Item */}
+            {/* EndGifs */}
         </>
     )
 }
