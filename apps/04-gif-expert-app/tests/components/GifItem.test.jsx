c58c113880897ba716e2fb084ae7d899e070b741
jeremyandes <jeremyandes25@gmail.com>
2022-08-19 21:04:21 -0300

    103. Pruebas en el helper getGifs

--- apps/04-gif-expert-app/tests/components/GifItem.test.jsx
+++ apps/04-gif-expert-app/tests/components/GifItem.test.jsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react"
 import { GifItem } from "../../src/components/GifItem"
 
-describe('Pruebas en <GifItem />', () => {
+describe('Testing <GifItem />', () => {
 
     const title = 'Michael Jordan';
     const url = 'https://media0.giphy.com/media/l3q2JCu9lep6dAmyY/giphy.gif?cid=4f32db46skaam7bco6t1ev32fy7dvl6l2227o7vualbk7pik&rid=giphy.gif&ct=g'
