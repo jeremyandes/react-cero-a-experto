265bc3aba0b748881ef3293e6090c136751b2fea
jeremyandes <jeremyandes25@gmail.com>
2022-09-01 20:48:24 -0300

    125. useFetch + useCounter

--- apps/05-hooks-app/src/03-examples/MultipleCustomHooks.jsx
+++ apps/05-hooks-app/src/03-examples/MultipleCustomHooks.jsx
@@ -5,9 +5,31 @@ export const MultipleCustomHooks = () => {
     const { data, isLoading, hasErrors } = useFetch(`${baseUrl}/quotes/1`);
     console.log({ data, isLoading, hasErrors });
 
+    const { quote, author } = !!data && data[0];
+
     return (
-        <>
+        <div className="container">
             <h1>Breaking Bad</h1>
-        </>
+            <hr />
+
+            {
+                (isLoading)
+                    ? (
+                        <div className="alert alert-info text-center w-100 mx-auto">
+                            <h5>Cargando...</h5>
+                        </div>
+                    )
+                    : (
+                        <blockquote className="blockquote text-end">
+                            <p className="mb-2">{quote}</p>
+                            <footer className="blockquote-footer">{author}</footer>
+                        </blockquote>
+                    )
+            }
+            
+            <button className="btn btn-outline-dark">
+                Next quote
+            </button>
+        </div>
     )
 }
