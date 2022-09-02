564d2dd882abdac15e7d83858f6c016732ef3312
jeremyandes <jeremyandes25@gmail.com>
2022-09-01 21:37:24 -0300

    129. useLayoutEffect

--- apps/05-hooks-app/src/03-examples/Quote.jsx
+++ apps/05-hooks-app/src/03-examples/Quote.jsx
@@ -1,11 +1,29 @@
 import PropTypes from 'prop-types';
+import { useLayoutEffect, useRef, useState } from 'react';
 
 export const Quote = ({ quote, author }) => {
+    const pRef = useRef();
+    const [boxSixe, setBoxSixe] = useState({ width: 0, height: 0 });
+
+    useLayoutEffect(() => {
+        const { width, height } = pRef.current.getBoundingClientRect();
+        setBoxSixe({ width, height });
+
+    }, [quote])
+
     return (
-        <blockquote className="blockquote text-end">
-            <p className="mb-2">{quote}</p>
-            <footer className="blockquote-footer">{author}</footer>
-        </blockquote>
+        <>
+            <blockquote className="blockquote text-end" style={{
+                display: 'flex',
+                alignItems: 'center',
+                gap: '15px',
+            }}>
+                <p ref={pRef} className="mb-2">{quote}</p>
+                <footer className="blockquote-footer">{author}</footer>
+            </blockquote>
+
+            <code>{JSON.stringify(boxSixe)}</code>
+        </>
     )
 }
 
--- apps/05-hooks-app/src/main.jsx
+++ apps/05-hooks-app/src/main.jsx
@@ -6,6 +6,7 @@ import { FormCustomHook } from './02-useEffect/FormCustomHook'
 import { SimpleForm } from './02-useEffect/SimpleForm'
 import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 import { FocusScreen } from './04-useRef/FocusScreen'
+import { Layout } from './05-useLayoutEffect/Layout'
 import { HooksApp } from './HooksApp'
 import './index.css'
 
@@ -17,6 +18,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     {/* <SimpleForm /> */}
     {/* <FormCustomHook /> */}
     {/* <MultipleCustomHooks /> */}
-    <FocusScreen />
+    {/* <FocusScreen /> */}
+    <Layout />
   </React.StrictMode>
 )
