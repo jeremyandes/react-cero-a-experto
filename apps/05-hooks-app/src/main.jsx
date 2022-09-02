97735e556b38d3c92bfea7f3b469394432c77a44
jeremyandes <jeremyandes25@gmail.com>
2022-09-01 21:58:22 -0300

    131. useMemo

--- apps/05-hooks-app/src/main.jsx
+++ apps/05-hooks-app/src/main.jsx
@@ -7,6 +7,7 @@ import { SimpleForm } from './02-useEffect/SimpleForm'
 import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 import { FocusScreen } from './04-useRef/FocusScreen'
 import { Layout } from './05-useLayoutEffect/Layout'
+import { MemoHook } from './06-memos/MemoHook'
 import { Memorize } from './06-memos/Memorize'
 import { HooksApp } from './HooksApp'
 import './index.css'
@@ -21,6 +22,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     {/* <MultipleCustomHooks /> */}
     {/* <FocusScreen /> */}
     {/* <Layout /> */}
-    <Memorize />
+    {/* <Memorize /> */}
+    <MemoHook />
   </React.StrictMode>
 )
