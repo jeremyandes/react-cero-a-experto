75b6ed311b856467ffb176d0d997fe50f1d12cb3
jeremyandes <jeremyandes25@gmail.com>
2022-09-01 21:48:46 -0300

    130. Memo - Método de React

--- apps/05-hooks-app/src/main.jsx
+++ apps/05-hooks-app/src/main.jsx
@@ -7,6 +7,7 @@ import { SimpleForm } from './02-useEffect/SimpleForm'
 import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 import { FocusScreen } from './04-useRef/FocusScreen'
 import { Layout } from './05-useLayoutEffect/Layout'
+import { Memorize } from './06-memos/Memorize'
 import { HooksApp } from './HooksApp'
 import './index.css'
 
@@ -19,6 +20,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     {/* <FormCustomHook /> */}
     {/* <MultipleCustomHooks /> */}
     {/* <FocusScreen /> */}
-    <Layout />
+    {/* <Layout /> */}
+    <Memorize />
   </React.StrictMode>
 )
