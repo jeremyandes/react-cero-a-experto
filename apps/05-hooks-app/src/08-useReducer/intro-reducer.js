9f9a099e128977e71a0c0e898c9a27f2b1c3f6ad
jeremyandes <jeremyandes25@gmail.com>
2022-09-11 12:56:04 -0300

    140. Idea general de un reducer - Via codigo

--- apps/05-hooks-app/src/08-useReducer/intro-reducer.js
+++ apps/05-hooks-app/src/08-useReducer/intro-reducer.js
@@ -1,1 +1,38 @@
-console.log('hola mundo');
+const initialState = [
+    {
+        id: 1,
+        todo: 'Aprender React completo',
+        done: false,
+    }
+];
+
+const todoReducer = (state = initialState, action = {}) => {
+    if (action.type === '[TODO] add todo') {
+        return [...state, action.payload];
+    }
+
+    return state;
+}
+
+
+let todos = todoReducer();
+// todo.push({
+//     id: 2,
+//     todo: 'Entender los Reducer',
+//     done: false,
+// })
+
+const newTodo = {
+    id: 2,
+    todo: 'Entender los Reducer',
+    done: false,
+};
+
+const addTodoAction = {
+    type: '[TODO] add todo',
+    payload: newTodo,
+}
+
+todos = todoReducer(todos, addTodoAction);
+
+console.log({ state: todos });
