05de69c6a88dd5c5d31664361d8be6c23c26cc3e
jeremyandes <jeremyandes25@gmail.com>
2022-09-19 20:17:47 -0300

    146. Guardar y Leer TODOs en LocalStorage

--- apps/05-hooks-app/src/08-useReducer/TodoApp.jsx
+++ apps/05-hooks-app/src/08-useReducer/TodoApp.jsx
@@ -2,25 +2,33 @@ import { useReducer } from "react"
 import { todoReducer } from "./todoReducer";
 import { TodoAdd } from "./components/TodoAdd";
 import { TodoList } from "./components/TodoList";
+import { useEffect } from "react";
 
-const initialState = [
-    {
-        id: 1,
-        description: 'Comprender el useReducer',
-        done: false,
-        timestamp: new Date().getTime(),
-    },
-    {
-        id: 2,
-        description: 'Comprender React',
-        done: false,
-        timestamp: new Date().getTime(),
-    },
-];
+// const initialState = [
+//     {
+//         id: 1,
+//         description: 'Comprender el useReducer',
+//         done: false,
+//         timestamp: new Date().getTime(),
+//     },
+//     {
+//         id: 2,
+//         description: 'Comprender React',
+//         done: false,
+//         timestamp: new Date().getTime(),
+//     },
+// ];
+
+
+const init = () => JSON.parse(localStorage.getItem('todos')) || [];
 
 export const TodoApp = () => {
+    const [todos, dispatch] = useReducer(todoReducer, [], init);
 
-    const [todos, dispatch] = useReducer(todoReducer, initialState);
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }, [todos])
+
 
     const handleNewTodo = (todo) => {
         const action = {
