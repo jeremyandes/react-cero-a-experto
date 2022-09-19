84a222b829d8f24442927fd82ac0ce38659df791
jeremyandes <jeremyandes25@gmail.com>
2022-09-19 20:10:54 -0300

    145. Agregar un nuevo TODO

--- apps/05-hooks-app/src/08-useReducer/components/TodoItem.jsx
+++ apps/05-hooks-app/src/08-useReducer/components/TodoItem.jsx
@@ -1,8 +1,7 @@
 export const TodoItem = ({ item }) => {
     return (
         <li key={item.id} className="list-group-item d-flex justify-content-between align-items-center">
-            <h5>Item {item.id}</h5>
-            <span>{item.description}</span>
+            <h5>{item.description}</h5>
             <button className="btn btn-danger">Borrar</button>
         </li>
     )
