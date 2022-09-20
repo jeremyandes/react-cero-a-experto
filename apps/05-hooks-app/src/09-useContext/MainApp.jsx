5ee155fef1f9b17ca9fadf9b980a1e86420ff934
jeremyandes <jeremyandes25@gmail.com>
2022-09-20 19:52:35 -0300

    155. Preparación de nuestra aplicación con rutas

---
+++ apps/05-hooks-app/src/09-useContext/MainApp.jsx
@@ -0,0 +1,8 @@
+export const MainApp = () => {
+    return (
+        <div className="container">
+            <h1>MainApp</h1>
+            <hr />
+        </div>
+    )
+}
--- apps/05-hooks-app/src/main.jsx
+++ apps/05-hooks-app/src/main.jsx
@@ -1,38 +1,11 @@
+import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-import './08-useReducer/intro-reducer';
-import { TodoApp } from './08-useReducer/TodoApp';
-
-import './index.css'
-// import { HooksApp } from './HooksApp'
-// import { Padre } from './07-tarea-memo/Padre'
-// import { MemoHook } from './06-memos/MemoHook'
-// import { Memorize } from './06-memos/Memorize'
-// import { Layout } from './05-useLayoutEffect/Layout'
-// import { CounterApp } from './01-useState/CounterApp'
-// import { FocusScreen } from './04-useRef/FocusScreen'
-// import { SimpleForm } from './02-useEffect/SimpleForm'
-// import { CallbackHook } from './06-memos/CallbackHook'
-// import { FormCustomHook } from './02-useEffect/FormCustomHook'
-// import { CounterCustomHook } from './01-useState/CounterCustomHook'
-// import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+import { BrowserRouter } from "react-router-dom";
+import { MainApp } from './09-useContext/MainApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  //   {/* <HooksApp /> */}
-  //   {/* <CounterApp /> */}
-  //   {/* <CounterCustomHook /> */}
-  //   {/* <SimpleForm /> */}
-  //   {/* <FormCustomHook /> */}
-  //   {/* <MultipleCustomHooks /> */}
-  //   {/* <FocusScreen /> */}
-  //   {/* <Layout /> */}
-  //   {/* <Memorize /> */}
-  //   {/* <MemoHook /> */}
-  //   {/* <CallbackHook /> */}
-  //   {/* <Padre /> */}
-  // </React.StrictMode>
-
-  <TodoApp />
+  <BrowserRouter>
+    <MainApp />
+  </BrowserRouter>
 )
